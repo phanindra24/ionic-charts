8117667d80f752892b6b7ed5e75bed9d7e925be2
phanindra24 <phanindraiitb@gmail.com>
2018-02-08 17:31:13 +0530

    added master-Details chart

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as HighCharts from 'highcharts';
 
 @Component({
   selector: 'page-contact',
@@ -9,6 +10,89 @@ export class ContactPage {
 
   constructor(public navCtrl: NavController) {
 
+  }
+  ionViewDidLoad() {
+    var myChart = HighCharts.chart('container-contact', {
+      chart: {
+        type: 'column',
+        zoomType: 'x'
+    },
+    title: {
+        text: 'Stacked bar chart'
+    },
+    xAxis: {
+        categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas'],
+        min:0
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Total fruit consumption'
+        }
+    },
+    legend: {
+        reversed: true
+    },
+    plotOptions: {
+        series: {
+            stacking: 'normal'
+        },
+        column:{
+          pointWidth:5
+        }
+    },
+    series: [{
+        name: 'John',
+        data: [5, 3, 4, 7, 2, 2, 3, 2, 1,2, 3, 2, 1]
+    }, {
+        name: 'Jane',
+        data: [2, 2, 3, 2, 1, 3, 4, 7, 2, 2, 3, 2, 1]
+    }, {
+        name: 'Joe',
+        data: [3, 4, 4, 2, 5, 3, 4, 7, 2, 2, 3, 2, 1]
+    }]
+    });
+
+    var timeline = HighCharts.chart('container-timeline', {
+      chart: {
+        type: 'bar',
+        zoomType: 'x'
+    },
+    title: {
+        text: 'Stacked bar chart'
+    },
+    xAxis: {
+        categories: ['Apples'],
+        min:0
+    },
+    yAxis: {
+        min: 0,
+        title: {
+            text: 'Total fruit consumption'
+        }
+    },
+    legend: {
+        reversed: true
+    },
+    plotOptions: {
+        series: {
+            stacking: 'normal'
+        },
+        bar:{
+          pointWidth:5
+        }
+    },
+    series: [{
+        name: 'John',
+        data: [5]
+    }, {
+        name: 'Jane',
+        data: [2]
+    }, {
+        name: 'Joe',
+        data: [3]
+    }]
+    });
   }
 
 }
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as HighCharts from 'highcharts';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,42 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+  // ionViewDidLoad() {
+  //   var myChart = HighCharts.chart('container', {
+  //     chart: {
+  //       type: 'column'
+  //   },
+  //   title: {
+  //       text: 'Stacked bar chart'
+  //   },
+  //   xAxis: {
+  //       categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+  //   },
+  //   yAxis: {
+  //       min: 0,
+  //       title: {
+  //           text: 'Total fruit consumption'
+  //       }
+  //   },
+  //   legend: {
+  //       reversed: true
+  //   },
+  //   plotOptions: {
+  //       series: {
+  //           stacking: 'normal'
+  //       }
+  //   },
+  //   series: [{
+  //       name: 'John',
+  //       data: [5, 3, 4, 7, 2]
+  //   }, {
+  //       name: 'Jane',
+  //       data: [2, 2, 3, 2, 1]
+  //   }, {
+  //       name: 'Joe',
+  //       data: [3, 4, 4, 2, 5]
+  //   }]
+  //   });
+  // }
 
 }
